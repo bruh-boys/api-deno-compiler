3221715eb7ba160e990d71f798d85d484f3a54e9
pai <hachikopolarcito321@gmail.com>
2021-07-05 02:23:09 -0500

    no overwrite files

--- routes/code.ts
+++ routes/code.ts
@@ -1,16 +1,15 @@
 import { Router } from "express";
 import { exec } from "child_process";
-import { writeFile } from "fs";
+import { unlink, writeFile } from "fs";
 const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 60, // limit 60 request for minute
-  message: ({out:"HTTP ERROR 429 to many requests"}),
+  message: ({ out: "HTTP ERROR 429 to many requests" }),
 });
 
 const router = Router();
-const deno = "./deno run --allow-net --no-check  execute.ts";
 
 const regex =
   /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
@@ -18,17 +17,28 @@ const regex =
 router.post("/code", limiter, async (req, res) => {
   const { code } = req.body;
 
-  writeFile("execute.ts", code, (err) => {
+  const randomfile = (`${Math.random()}.ts`);
+  const deno = `./deno run --allow-net --no-check  ${randomfile}`;
+  // create the file
+  writeFile(randomfile, code, (err) => {
     if (err) {
       console.error(err);
       return;
     }
   });
+  // exec the code
   exec(deno, { timeout: 2000 }, (_, stdout, stderr) => {
     let out = (stdout || stderr).replace(regex, "");
     console.log(out);
-    res.json({
+    res.json({ // send the output
       out: out,
+    });
+    // delete the file
+    unlink(randomfile, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
     });
   });
 });
